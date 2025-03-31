6512b1791384222755cc88c5f53ffd6ef7bf8373
Lee Faus <lfaus@gitlab.com>
2025-03-31 14:36:09 -0400

    add UI styling for login components

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def components
+    # This action renders the components showcase page
+    # No authentication required for this demo page
+  end
 end
